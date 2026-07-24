1544b480c4dc000696048dc4de773a3f4b94de48
apososgol1000-png <apososgol1000@gmail.com>
2026-07-24 09:00:20 -0600

    Add files via upload

---
+++ src/components/SceneErrorBoundary.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+export default class SceneErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    console.error("Scene3D fallo, mostrando fallback:", error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+    return this.props.children;
+  }
+}
